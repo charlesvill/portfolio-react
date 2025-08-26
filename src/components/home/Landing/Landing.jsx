25e259ab5f83842bb308f7dab441107e868af5e4
Charles Villalpando <charlesvill@g.ucla.edu>
2025-08-25 23:40:00 -0700

    Styling for landing

--- src/components/home/Landing/Landing.jsx
+++ src/components/home/Landing/Landing.jsx
@@ -2,30 +2,39 @@ import styles from './landing.module.css';
 
 export function Landing() {
   return (
-    <section className="landing">
+    <header className="landing">
       <div className={styles.greetCont}>
-        <div>
-          <h3>H!</h3>
-          <h4>My name is Charles Villalpando</h4>
-        </div>
-        <div>
-          <span>I write and design full stack web applications</span>
-        </div>
+        <h1>My name is Charles Villalpando</h1>
+        <p>
+          Hello and welcome! I write and design full stack web applications.
+        </p>
       </div>
+
       <div className={styles.navCont}>
-        <nav>
-          <a href="#about">Summary</a>
-          <a href="#skills">Skills</a>
-          <a href="#experience">Experience</a>
-          <a href="#education">Education</a>
+        <nav aria-label="Home sections">
+          <ul>
+            <li>
+              <a href="#about">Summary</a>
+            </li>
+            <li>
+              <a href="#skills">Skills</a>
+            </li>
+            <li>
+              <a href="#experience">Experience</a>
+            </li>
+            <li>
+              <a href="#education">Education</a>
+            </li>
+          </ul>
         </nav>
+
         <div className={styles.imgCont}>
           <img
             src="/static/images/stylizedPortrait.png"
-            alt="stylized picture of Charles"
+            alt="Stylized portrait of Charles Villalpando"
           />
         </div>
       </div>
-    </section>
+    </header>
   );
 }
