f5d987ccd4bd7991b91c7749710fb29cb374ef53
Charles Villalpando <charlesvill@g.ucla.edu>
2025-09-03 21:13:58 -0700

    Skills section & styling

--- src/components/home/skills/skills.jsx
+++ src/components/home/skills/skills.jsx
@@ -1,16 +1,50 @@
+import {
+  SiReact,
+  SiJavascript,
+  SiExpress,
+  SiNodedotjs,
+  SiPostgresql,
+  SiPrisma,
+  SiCloudinary,
+  SiKoyeb,
+} from 'react-icons/si';
+import styles from './skills.module.css';
+
 export function Skills() {
   return (
     <section id="skills">
       <h5>Technologies I speak:</h5>
-      <ul>
-        <li>React</li>
-        <li>Javascript</li>
-        <li>Express</li>
-        <li>Nodejs</li>
-        <li>PostgresSql</li>
-        <li>Prisma</li>
-        <li>Cloudinary</li>
-        <li>Koyebd</li>
+      <ul className={styles.skillList}>
+        <li>
+          <SiReact /> React
+        </li>
+        <li>
+          <SiJavascript /> Javascript
+        </li>
+        <li>
+          <SiExpress />
+          Express
+        </li>
+        <li>
+          <SiNodedotjs />
+          Nodejs
+        </li>
+        <li>
+          <SiPostgresql />
+          PostgresSql
+        </li>
+        <li>
+          <SiPrisma />
+          Prisma
+        </li>
+        <li>
+          <SiCloudinary />
+          Cloudinary
+        </li>
+        <li>
+          <SiKoyeb />
+          Koyebd
+        </li>
       </ul>
     </section>
   );
