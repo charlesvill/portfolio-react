5d3ae049b8c8f0ef028c8f57e081eb9bf997813c
Charles Villalpando <charlesvill@g.ucla.edu>
2025-09-05 16:11:04 -0700

    Experience refactor to object mapper

--- src/components/home/experience/experience.jsx
+++ src/components/home/experience/experience.jsx
@@ -1,71 +1,106 @@
+import { useState } from 'react';
 import styles from './experience.module.css';
 
+const workData = {
+  softwareDevelopment: {
+    primaryHeading: 'Software Development',
+    content: [
+      {
+        title: 'Odin Project',
+        list: [
+          'Front-end and serverside development and deployment',
+          'Clear, maintainable code following modern Object Oriented design patterns',
+          'Speciality in designing REST API backends with relational database for CRUD functions',
+          'Secure user authentication with JSON Web Tokens that is CDN deployment ready',
+          'Designed, developed over 20 projects prioritizing vanilla js,html, css, react to build strong foundations for solving problems and designing critical software infastructure',
+        ],
+      },
+    ],
+  },
+  education: {
+    primaryHeading: 'Education',
+    content: [
+      {
+        title: 'Special Education Case Manager - Total Education Systems',
+        list: [
+          'Review, Plan, and Develop IEPs for over 95 students accross * campuses in the LA region ',
+          'Prioritized high-impact tasks and streamlined processes for efficiency in meeting a high volume of cases',
+        ],
+      },
+      {
+        title:
+          'Special Education Teacher - Alliance College Ready Public Schools',
+        list: [
+          'Specialist in tailoring and accommodating curriculum to provide access to scholars with diverse learning needs',
+          'Collaborated closely with team of stakeholders for each scholar unifying on education plan and methodology to refactor ed plan',
+        ],
+      },
+    ],
+  },
+};
+
+function TabButton({ heading, currentArticle, handleSelection }) {
+  return (
+    <button
+      onClick={handleSelection}
+      id={heading}
+      className={`${styles.heading} ${currentArticle === heading ? styles.active : ''}`}
+    >
+      {workData[heading].primaryHeading}
+    </button>
+  );
+}
+function ArticleComponent({ data }) {
+  return (
+    <div className={styles.contentWrapper}>
+      <h5>{data.title}</h5>
+      <ul className={styles.contentlist}>
+        {data.content.map((listItem, index) => (
+          <li className={styles.listItem} key={index}>
+            {listItem}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
 export function Experience() {
+  const [currentArticle, setArticle] = useState('softwareDevelopment');
+  // const tabHeadings = Object.entries(workData).map(([key, prop]) => prop.primaryHeading);
+  const dataKeys = Object.keys(workData);
+  const articleContent = workData[currentArticle].content;
+
+  function handleSelection(e) {
+    const selection = e.target.id;
+    if (currentArticle === selection) {
+      return;
+    }
+
+    setArticle(selection);
+  }
+
+  // pulls the data for current article and creates component.  expects data object to have  key with the name stored in currentArticle state
   return (
     <section id="experience">
       <header>
         <h3>Experience</h3>
       </header>
       <article className={styles.articleCont}>
-        <h4>Software Development</h4>
-        <ul>
-          <li>
-            <h5>Odin Project</h5>
-            <ul>
-              <li>Front-end and serverside development and deployment</li>
-              <li>
-                Clear, maintainable code following modern Object Oriented design
-                patterns
-              </li>
-              <li>
-                Speciality in designing REST API backends with relational
-                database for CRUD functions{' '}
-              </li>
-              <li>
-                Secure user authentication with JSON Web Tokens that is CDN
-                deployment ready
-              </li>
-              <li>
-                Designed, developed over 20 projects prioritizing vanilla js,
-                html, css, react to build strong foundations for solving
-                problems and designing critical software infastructure
-              </li>
-            </ul>
-          </li>
-        </ul>
-      </article>
-      <article className={styles.articleCont}>
-        <h4>Education</h4>
-        <ul>
-          <li>
-            <h5>Special Education Case Manager - Total Education Systems</h5>
-            <ul>
-              <li>
-                Review, Plan, and Develop IEPs for over 95 students accross 8
-                campuses in the LA region
-              </li>
-              <li>
-                Prioritized high-impact tasks and streamlined processes for
-                efficiency in meeting a high volume of cases
-              </li>
-            </ul>
-          </li>
-          <li>
-            <h5>
-              Special Education Teacher - Alliance College Ready Public Schools
-            </h5>
-            <ul>
-              <li>
-                Specialist in tailoring and accommodating curriculum to provide
-                access to scholars with diverse learning needs
-              </li>
-              <li>
-                Collaborated closely with team of stakeholders for each scholar
-                unifying on education plan and methodology to refactor ed plan
-              </li>
-            </ul>
-          </li>
-        </ul>
+        <div className={styles.tabBox}>
+          {dataKeys.map((heading) => (
+            <TabButton
+              heading={heading}
+              currentArticle={currentArticle}
+              handleSelection={handleSelection}
+              key={heading}
+            />
+          ))}
+        </div>
+        <div className={styles.contentBox}>
+          {articleContent.map((element) => (
+            <ArticleComponent data={element} key={element.title} />
+          ))}
+        </div>
       </article>
     </section>
   );
